816cdec0055a7a070e057c120ca09f11d5367824
Jain <paryjain@deloitte.com>
2019-11-05 23:41:41 +0530

    added Readme

--- src/main/java/io/thetechthirsty/springsecurityjwt/service/MyUserDetailsService.java
+++ src/main/java/io/thetechthirsty/springsecurityjwt/service/MyUserDetailsService.java
@@ -13,7 +13,7 @@ public class MyUserDetailsService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return new User("foo1", "$2a$10$PqbwFOkdK.X1gZ3dxmcgwuh8w7jeC.3uhBMkADNEYuf4avBVN57B2", new ArrayList<>());
+		return new User("foo", "$2a$10$PqbwFOkdK.X1gZ3dxmcgwuh8w7jeC.3uhBMkADNEYuf4avBVN57B2", new ArrayList<>());
 	}
 
 }
